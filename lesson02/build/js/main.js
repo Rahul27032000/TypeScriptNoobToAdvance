ece8b721dc9b8e605809dc2d10a53f352e0cfab5
Shinde Rahul <shinde2732000@gmail.com>
2023-08-06 13:48:57 +0530

    commit after lesson 2

---
+++ lesson02/build/js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let username = " Rahul";
+console.log(username);
+let a = 12;
+let b = "6";
+let c = true;
---
+++ lesson02/src/test.ts
@@ -0,0 +1,14 @@
+let myName: string = "Rahul";
+let myAge: number = 23;
+let isLoading: boolean = false;
+let album: string | number;
+
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+// union types
+let postId: number | string = 0;
+let isActive: boolean | number = false;
+
+let re: RegExp = /\w+/g;
